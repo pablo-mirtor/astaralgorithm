3ee927385a5a141496e0919a56dcff5fe31bc0e8
Pablo <pmiran01@ucm.es>
2020-04-26 19:14:26 +0200

    hola

---
+++ src/app/tablewaypoints/tablewaypoints.component.ts
@@ -0,0 +1,76 @@
+import {Component, OnInit} from '@angular/core';
+import {CellState} from '../cell-state.enum';
+import {Node} from '../node';
+import {TableServiceService} from '../table-service.service';
+
+@Component({
+  selector: 'app-table-way-points',
+  templateUrl: './tablewaypoints.component.html',
+  styleUrls: ['./tablewaypoints.component.css']
+})
+export class TableWayPointsComponent implements OnInit {
+  nodeTable = this.tableService.createNewTable(9, 9);
+  numRows: number;
+  numColumns: number;
+  clickEnableWayPoint: boolean;
+  restartButton: boolean = false;
+  cost: number;
+  wayPoints: Node[] = new Array<Node>();
+
+  constructor(private tableService: TableServiceService) {
+    this.numRows = 9;
+    this.numColumns = 9;
+    this.restartButton = false;
+  }
+
+  ngOnInit(): void {
+  }
+
+  cellClicked(node: Node): void{
+    if(this.clickEnableWayPoint) {
+      node.setState(CellState.wayPoint);
+      this.wayPoints.push(node);
+      this.clickEnableWayPoint = false;
+      return;
+    }
+    if(node.getState() === CellState.empty) {
+      node.setState(CellState.forbidden);
+      return;
+    }
+    if(node.getState() === CellState.wayPoint) {
+      this.wayPoints.splice(this.wayPoints.indexOf(node), 1);
+    }
+
+    node.setState(CellState.empty);
+  }
+
+
+  updateTable(): void{
+    if(this.numRows >= 3 && this.numRows <= 9 && this.numColumns >= 3 && this.numColumns <= 9)
+      this.nodeTable = this.tableService.createNewTable(this.numRows, this.numColumns);
+    else
+      alert("El mínimo de filas/columnas es 3 y el máximo 10");
+  }
+
+  iniEnable(): void{
+    this.clickEnableWayPoint = true;
+  }
+
+
+  solve(): void{
+    if(this.wayPoints.length < 2)
+      alert("Selecciona mínimo dos Way Points");
+    else {
+      this.cost = this.tableService.solveWayPoints(this.wayPoints);
+      this.restartButton = true;
+      if(this.cost === 0)
+        alert("No hay solución");
+    }
+  }
+
+  restart(): void{
+    this.updateTable();
+    this.wayPoints = new Array<Node>();
+    this.restartButton = false;
+  }
+}
